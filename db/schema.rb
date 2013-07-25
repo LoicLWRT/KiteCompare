57ea5d463623da9cc7af0b817a2a6895e1ba4db5
Loic Lwrt <loic@lwrt.fake>
2013-07-26 00:40:48 +0200

    Suppression de la dernière base de donnée

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130725223730) do
-
-  create_table "materiels", force: true do |t|
-    t.string   "nom"
-    t.string   "type"
-    t.integer  "annee"
-    t.integer  "prix_neuf"
-    t.integer  "prix_occasion"
-    t.text     "description_avantages"
-    t.text     "description_inconvenients"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
