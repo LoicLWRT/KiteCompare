19c6e73fa5f1e237fcea472863d552687afb85bd
Loic Lwrt <loic@lwrt.fake>
2013-07-26 00:23:29 +0200

    Ajout de la base du code Ruby on Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KiteCompare::Application.config.secret_key_base = '44743441710115908312edd3e4f618bd62148c6497d7801cdb4ce6c334ed6a5ac0827461c96efadd1a34f4ad7e46f4d666756d515f7a72a6ba2c2bd05d959524'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KiteCompare::Application.config.session_store :cookie_store, key: '_KiteCompare_session'
