c7349f565914f32762dfb9c2f50b7a8b829d20ce
Loic Lwrt <loic@lwrt.fake>
2013-07-27 00:32:19 +0200

    Added a DB seed generator

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Marque.create(name: 'Naish')
+Marque.create(name: 'F.One')
+
+MaterielType.create(name: Kite)
+MaterielType.create(name: Board)
+MaterielType.create(name: Bar)
+
+Materiel.create(materieltype_id:1 modele:'Bandit 5' marque_id:2 annee:2012 url_image:string prix_neuf:800 prix_occasion:550 description_avantages:'Rapide' description_inconvenients:'Casse facilement')
