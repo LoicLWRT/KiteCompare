5b3c72ca2c74ca82679db4fa56730ab177ba842e
Loic Lwrt <loic@lwrt.fake>
2013-07-30 00:47:38 +0200

    Amélioration du script avec compatibilité jpg et png

--- scripts/images_batch_resize.sh
+++ scripts/images_batch_resize.sh
@@ -2,9 +2,9 @@
 
 cd kites_images/
 
-function copy_file
+function copy_file_jpg
 {
-	echo "Le fichier traité est $1"
+	echo "Le fichier JPG traité est $1"
 	file_name=$1
 	file_name=`echo ${file_name%????}`
 	file_name=${file_name:2}
@@ -12,12 +12,33 @@ function copy_file
 	cp "$file_name.jpg" ../../app/assets/images/kites/"$file_name-small.jpg"
 	cp "$file_name.jpg" ../../app/assets/images/kites/"$file_name-large.jpg"
 	
-	echo "Operation finie pour $1"
+	echo "Operation finie pour le JPG $1"
+}
+
+function copy_file_png
+{
+	echo "Le fichier PNG traité est $1"
+	file_name=$1
+	file_name=`echo ${file_name%????}`
+	file_name=${file_name:2}
+	
+	cp "$file_name.jpg" ../../app/assets/images/kites/"$file_name-small.png"
+	cp "$file_name.jpg" ../../app/assets/images/kites/"$file_name-large.png"
+	
+	echo "Operation finie pour le PNG $1"
 }
 
 export -f copy_file
-find . -name "*.jpg" -exec bash -c 'copy_file "$0"' {} \;
+find . -name "*.jpg" -exec bash -c 'copy_file_jpg "$0"' {} \;
+find . -name "*.png" -exec bash -c 'copy_file_png "$0"' {} \;
 
 cd ../../app/assets/images/kites/
 sips -Z 200 *-small.jpg
 sips -Z 600 *-large.jpg
+
+sips -Z 200 --setProperty format jpeg *-small.png --out *-small.jpg
+sips -Z 600 --setProperty format jpeg *-large.png --out *-large.jpg
+
+for file in *.png ; do mv $file `echo $file | sed 's/\(.*\.\)png/\1jpg/'` ; done
+
+
