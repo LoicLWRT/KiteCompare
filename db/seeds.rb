fb6b2560146d26d1794b3a1567bb2ed1a54a64a7
tobios <contact@tobios.org>
2013-07-30 23:19:17 +0200

    Ajout du caroussel sur la page contact + petites modifs sur seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,6 +15,7 @@ Marque.create(name: 'Genetrix')
 Marque.create(name: 'Nobile')
 Marque.create(name: 'Gaastra')
 Marque.create(name: 'Slingshot')
+Marque.create(name: 'Ozone')
 
 # Ailes
 
@@ -25,7 +26,7 @@ CritiqueAile.create(type_critique_id:2, source:'Forum Flysurf', lien:'http://for
 
 # Bandit 6
 Aile.create(modele:'Bandit 6', marque_id:2, annee:2013, note:5)
-CritiqueAile.create(type_critique_id:1, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2474', contenu:'Des améliorations en déhooké par rapport à la version précédente.', aile_id:2)
+CritiqueAile.create(type_critique_id:1, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2474', contenu:'Des améliorations au niveau du déhooké par rapport à la version précédente.', aile_id:2)
 PrixSurShop.create(nom_shop:'Freeride Attitude', lien_produit:'http://www.freeride-attitude.com/fone-bandit-6-2013,19829.html', prix_sans_barre:945, surface:10, aile_id:2)
 PrixSurShop.create(nom_shop:'Surf\'n Kite', lien_produit:'http://shop.surfnkite.fr/ailes-de-kitesurf/1129-fone-bandit-6.html', prix_sans_barre:944, surface:10, aile_id:2)
 PrixSurShop.create(nom_shop:'FranceKiteShop', lien_produit:'http://www.francekiteshop.com/ailes-de-kitesurf-f-one/135-f-one-bandit-6-2013-.html', prix_sans_barre:1180, surface:10, aile_id:2)
@@ -46,7 +47,9 @@ PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetven
 
 # Evo 2013
 Aile.create(modele:'Evo', marque_id:3, annee:2013, note:5)
-CritiqueAile.create(type_critique_id:1, source:'Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-evo-2013/', contenu:'Aile la plus polyvalente de la gamme North, s\'adapte a tous les domaines.', aile_id:4)
+CritiqueAile.create(type_critique_id:3, source:'Kiteforum.com', lien:'http://www.kiteforum.com/viewtopic.php?f=1&t=2379687&start=0', contenu:'Si vous naviguez avec des vagues et/ou des rafales ou si vous avez besoins de souvent faire redécoller le kite, préférez la Evo.', aile_id:4)
+CritiqueAile.create(type_critique_id:1, source:'Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-evo-2013/', contenu:'Aile la plus polyvalente de la gamme North, s\'adapte à tous les domaines.', aile_id:4)
+CritiqueAile.create(type_critique_id:1, source:'Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-evo-2013/', contenu:'Redécollage très facile, presque automatique.', aile_id:4)
 CritiqueAile.create(type_critique_id:2, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2797', contenu:'Léger temps de décalage dans les commandes.', aile_id:4)
 CritiqueAile.create(type_critique_id:3, source:'Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-evo-2013/', contenu:'Le pivot est mieux qu\'une Rebel, mais moins bien qu\'une Vegas.', aile_id:4)
 PrixSurShop.create(nom_shop:'Freeride Attitude', lien_produit:'http://www.freeride-attitude.com/north-evo-2013,20823.html', prix_sans_barre:1249, surface:10, aile_id:4)
@@ -65,6 +68,8 @@ PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetven
 PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetvent.com/6,0,north,evo,nue,aile,kite,2013-153831-40201.html', prix_sans_barre:1009, surface:6, aile_id:4)
 
 # Fuse 2013
+CritiqueAile.create(type_critique_id:1, source:'Kiteforum.com', lien:'http://www.kiteforum.com/viewtopic.php?t=2379687&p=787189', contenu:'Il est très facile de remonter au vent avec la Fuse.', aile_id:5)
+CritiqueAile.create(type_critique_id:3, source:'Kiteforum.com', lien:'http://www.kiteforum.com/viewtopic.php?f=1&t=2379687&start=0', contenu:'Si vous naviguez sur de l\'eau plate, ou avec du vent faible, ou si vous voulez sauter haut, essayez la Fuse.', aile_id:5)
 Aile.create(modele:'Fuse', marque_id:3, annee:2013, note:4)
 PrixSurShop.create(nom_shop:'Freeride Attitude', lien_produit:'http://www.freeride-attitude.com/north-fuse-2013,23113.html', prix_sans_barre:1279, surface:10, aile_id:5)
 PrixSurShop.create(nom_shop:'Airwave-shop', lien_produit:'http://www.airwave-shop.com/kitesurf/ailes-kitesurf/2013-north-fuse.html', prix_sans_barre:1279, surface:10, aile_id:5)
@@ -72,8 +77,8 @@ PrixSurShop.create(nom_shop:'Zeph Control', lien_produit:'http://www.zephcontrol
 
 # Rebel 2013
 Aile.create(modele:'Rebel', marque_id:3, annee:2013, note:3)
-CritiqueAile.create(type_critique_id:1, source:'Commentaire sur Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-rebel-2013/', contenu:'Le kite tire trop sur la barre pour faire du déhooké.', aile_id:6)
-CritiqueAile.create(type_critique_id:2, source:'Commentaire sur Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-rebel-2013/', contenu:'L\'aile a perdu en plage basse : il est donc plus difficile de naviguer en vent faible.', aile_id:6)
+CritiqueAile.create(type_critique_id:1, source:'Commentaire sur Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-rebel-2013/', contenu:'Le kite tire trop sur la barre pour être à l\'aise en déhooké.', aile_id:6)
+CritiqueAile.create(type_critique_id:2, source:'Commentaire sur Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-rebel-2013/', contenu:'L\'aile a perdu en plage basse cette année: il est donc plus difficile de naviguer en vent faible.', aile_id:6)
 CritiqueAile.create(type_critique_id:2, source:'Commentaire sur Kite-Spirit', lien:'http://www.kite-spirit.com/info/test-north-rebel-2013/', contenu:'L\'aile perd en réactivitée si on l\'utilise avec une barre North 2012.', aile_id:6)
 PrixSurShop.create(nom_shop:'Freeride Attitude', lien_produit:'http://www.freeride-attitude.com/north-rebel-2013,18933.html', prix_sans_barre:1279, surface:10, aile_id:6)
 PrixSurShop.create(nom_shop:'Airwave-shop', lien_produit:'http://www.airwave-shop.com/kitesurf/ailes-kitesurf/2013-north-rebel-nue.html', prix_sans_barre:1279, surface:10, aile_id:6)
@@ -93,13 +98,11 @@ PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetven
 PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetvent.com/5,0,north,rebel,aile,kite,2013,nue-141359-40201.html', prix_sans_barre:969, surface:5, aile_id:6)
 
 
-
-
 # Evo 2012
 Aile.create(modele:'Evo', marque_id:3, annee:2012, note:5)
 CritiqueAile.create(type_critique_id:1, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2274', contenu:'Redécollage très facile, presque automatique.', aile_id:7)
 CritiqueAile.create(type_critique_id:1, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2274', contenu:'L\'aile est assez polyvalente.', aile_id:7)
-CritiqueAile.create(type_critique_id:2, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2274', contenu:'La Evo a une grande pression en barre par rapport a la Rebel.', aile_id:7)
+CritiqueAile.create(type_critique_id:2, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2274', contenu:'La Evo a une grande pression en barre par rapport à la Rebel.', aile_id:7)
 CritiqueAile.create(type_critique_id:2, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2274', contenu:'Aile très physique en plage haute.', aile_id:7)
 CritiqueAile.create(type_critique_id:1, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2274', contenu:'La plage basse est vraiment large : on peut donc mieux naviguer en sous-toilé.', aile_id:7)
 PrixSurShop.create(nom_shop:'Neway Leucate', lien_produit:'http://www.neway-leucate.com/fr/shop/fiche_produit/ailes_kite-north-evo_2012-ref/?ref_produit=590911&PHPSESSID=b38b1da856becf50ad628da8d218a744', prix_sans_barre:1199, surface:10, aile_id:7)
@@ -127,11 +130,15 @@ PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetven
 
 # Neo 2012
 Aile.create(modele:'Neo', marque_id:3, annee:2012, note:4)
+CritiqueAile.create(type_critique_id:1, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2352', contenu:'En surf l\'aile est très bien optimisée.', aile_id:11)
+CritiqueAile.create(type_critique_id:2, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2352', contenu:'L\'aile n\'est pas faite pour le freestyle mais plutot pour le wakestyle : vous aurez du mal à monter haut en saut.', aile_id:11)
+CritiqueAile.create(type_critique_id:3, source:'Forum Kite-Spirit', lien:'http://www.kite-spirit.com/forum/viewtopic.php?id=2352', contenu:'Le redécollage fonctionne très bien, même si la Evo domine encore le secteur.', aile_id:11)
 PrixSurShop.create(nom_shop:'Neway Leucate', lien_produit:'http://www.neway-leucate.com/fr/shop/fiche_produit/ailes_kite-north-neo_2012-ref/?ref_produit=590936&url_retour=L2ZyL3Nob3AvbGlzdGVfcHJvZHVpdC9raXRlLWFpbGVzX2tpdGUtYWlsZXNfa2l0ZS1yZWYvP3JlZl9zc2ZhbWlsbGU9MDIwMjAx', prix_sans_barre:1229, surface:10, aile_id:11)
 PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetvent.com/12,0,north,neo,kite,nue,2012-122873-40202.html', prix_sans_barre:789, surface:12, aile_id:11)
 
 # Neo 2013
 Aile.create(modele:'Neo', marque_id:3, annee:2013, note:4)
+CritiqueAile.create(type_critique_id:1, source:'Kiteforum.com', lien:'http://www.kiteforum.com/viewtopic.php?t=2380605&p=795073', contenu:'Le depower est très impressionnant : on peut mettre ou enlever de la puissance simpl.', aile_id:4)
 PrixSurShop.create(nom_shop:'Neway Leucate', lien_produit:'http://www.neway-leucate.com/fr/shop/fiche_produit/ailes_kite-north-vegas_2012-ref/?ref_produit=590851&url_retour=L2ZyL3Nob3AvbGlzdGVfcHJvZHVpdC9raXRlLWFpbGVzX2tpdGUtYWlsZXNfa2l0ZS1yZWYvP3JlZl9zc2ZhbWlsbGU9MDIwMjAx', prix_sans_barre:1229, surface:10, aile_id:12)
 PrixSurShop.create(nom_shop:'Zeph Control', lien_produit:'http://www.zephcontrol.com/neo-2013-p-1044.html', prix_sans_barre:1129, surface:10, aile_id:12)
 PrixSurShop.create(nom_shop:'Vague et Vent', lien_produit:'http://www.vagueetvent.com/10,0,north,neo,kite,nue,2013-173520-40201.html', prix_sans_barre:1129, surface:10, aile_id:12)
