ff1ad9b36bc19734f2e78b5950c6a212780224ce
Loic Lwrt <loic@lwrt.fake>
2013-08-01 00:53:58 +0200

    Protection basique pour les marques

--- app/controllers/marques_controller.rb
+++ app/controllers/marques_controller.rb
@@ -1,4 +1,6 @@
 class MarquesController < ApplicationController
+  http_basic_authenticate_with :name => "", :password => "les3petitscochons", :except => [:index, :show]
+  
   before_action :set_marque, only: [:show, :edit, :update, :destroy]
 
   # GET /marques
