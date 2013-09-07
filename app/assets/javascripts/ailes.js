d0f4c06e67c968ad669e80dddebdf7208b86a3ef
Loic Lwrt <loic@lwrt.fake>
2013-09-07 18:08:27 +0200

    JS for compare button finished but ugly

--- app/assets/javascripts/ailes.js
+++ app/assets/javascripts/ailes.js
@@ -4,9 +4,33 @@
 
 var ailes_id = new Array();
 
-function testchecked(checkbox) 
+function check_box(id) 
 {
-	document.getElementById('mydiv').innerHTML = '<a class="btn btn-medium btn-primary" onclick="redirect_to_comparison()">Comparer les ailes sélectionnées</h2>';
+	//alert(id);
+	if (document.getElementsByName(id)[0].checked == false) {
+		document.getElementsByName(id)[0].checked = true;
+	}
+	else{
+		document.getElementsByName(id)[0].checked = false;
+	}
+	
+	testchecked(document.getElementsByName(id)[0]);
+}
+
+function hack_click(checkbox) 
+{
+	if (checkbox.checked == false) {
+		checkbox.checked = true;
+	}
+	else{
+		checkbox.checked = false;
+	}
+
+}
+
+function testchecked(checkbox) 
+{	
+	document.getElementById('mycont').innerHTML = '<div class="mydiv">  <a class="btn btn-medium btn-success" onclick="redirect_to_comparison()">Comparer les ailes sélectionnées</a> </div>';
 	
 	var index = ailes_id.indexOf(checkbox.name);
 	if (index != -1)
@@ -17,6 +41,16 @@ function testchecked(checkbox)
 	{
 		ailes_id.push(checkbox.name);
 	}
+	
+	if (ailes_id.length==0)
+	{
+		document.getElementById('mycont').innerHTML = '<div class="mydiv"><div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button> <p>Veuillez sélectionner au moins une aile</p></div></div>';
+	}
+	
+	if (ailes_id.length>4)
+	{
+		document.getElementById('mycont').innerHTML = '<div class="mydiv"><div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button> <p>Veuillez sélectionner au maximum 4 ailes</p></div></div>';
+	}
 		
 };
 
@@ -25,7 +59,7 @@ function redirect_to_comparison()
 	switch(ailes_id.length)
 	{
 	case 0:
-		alert("Vous devez choisir au moins une aile.")
+		document.getElementById('mycont').innerHTML = '<div class="mydiv"><div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button> <p>Veuillez sélectionner au moins une aile</p></div></div>';
 	break;
 	
 	case 1:
@@ -43,6 +77,11 @@ function redirect_to_comparison()
 	case 4:
 		url = "/comparer_ailes" + "?aile1=" + ailes_id[0] + "&aile2=" + ailes_id[1] + "&aile3=" + ailes_id[2] + "&aile4=" + ailes_id[3]
 	break;	
+	}
+	
+	if (ailes_id.length>4)
+	{
+		document.getElementById('mycont').innerHTML = '<div class="mydiv"><div class="alert alert-dismissable alert-warning"><button type="button" class="close" data-dismiss="alert">×</button> <p>Veuillez sélectionner au maximum 4 ailes</p></div></div>';
 	}
 	
 	if ((ailes_id.length>0) && (ailes_id.length<5))
