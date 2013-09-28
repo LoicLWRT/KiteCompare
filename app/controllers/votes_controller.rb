bf1b2f716b1f40632c65ab5a26db1df0d10d9406
Loic Lwrt <loic@lwrt.fake>
2013-09-28 08:36:17 +0200

    Début du sytème de vote

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -2,7 +2,6 @@ class VotesController < ApplicationController
   def create
     @aile = Aile.find(params[:aile_id])
 
-
     @vote = Vote.new(:aile_id => @aile.id, :ip_address => request.remote_ip.to_s, :user_agent => request.env["HTTP_USER_AGENT"].to_s )
     
     @vote.save
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,5 @@
 class Vote < ActiveRecord::Base
   belongs_to :aile
+  
+  validates_uniqueness_of :aile_id, :scope => [:ip_address, :user_agent]
 end
