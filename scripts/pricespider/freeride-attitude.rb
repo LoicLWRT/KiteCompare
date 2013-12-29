d35136a5c71f974c8108323745aebf2bb86203a6
Loic Lwrt <loic@lwrt.fake>
2013-12-29 15:03:21 +0100

    Fixed auto/manual in form view of prixsurshops

--- scripts/pricespider/freeride-attitude.rb
+++ scripts/pricespider/freeride-attitude.rb
@@ -5,40 +5,42 @@ require 'rubygems'
 require 'watir-webdriver'
 require 'nokogiri'
 
+surfaces = [
+  "5 m²",
+  "6 m²",
+  "7 m²",
+  "8 m²",
+  "9 m²",
+  "10 m²",
+  "11 m²",
+  "12 m²",
+  "13 m²",
+  "14 m²",
+  "15 m²",
+]
+
+def getprice(surfaces)
+  return prix
+end
+
+
 browser = Watir::Browser.new
 browser.goto 'http://www.freeride-attitude.com/north-kiteboarding-dice-2014,25701.html'
 
-browser.button(:id => '4-25691-25701').click
-  doc = Nokogiri::HTML.parse(browser.html)
-  doc.xpath('//*[@id="prix"]').each do |link|
-    puts link.content
+
+surfaces.each do |surface|
+  puts surface
+
+
+  begin
+    browser.button(:value => surface).click
+    doc = Nokogiri::HTML.parse(browser.html)
+    doc.xpath('//*[@id="prix"]').each do |link|
+      puts link.content
+    end
+
+  rescue StandardError=>e
+    puts "Could not get prices for #{surface}"  
   end
 
-browser.button(:id => '2-25691-25701').click
-  doc = Nokogiri::HTML.parse(browser.html)
-  doc.xpath('//*[@id="prix"]').each do |link|
-    puts link.content
-  end
-
-
-
-
-browser.goto 'http://www.surfavenuelarochelle.com/-de-9-m/1358-north-evo-2013-nue.html'
-
-browser.select_list(:name => 'group_3').select '11 m²'
-  doc = Nokogiri::HTML.parse(browser.html)
-  doc.xpath('//*[@id="our_price_display"]').each do |link|
-    puts link.content
-  end
-  
-browser.select_list(:name => 'group_3').select '12 m²'
-  doc = Nokogiri::HTML.parse(browser.html)
-  doc.xpath('//*[@id="our_price_display"]').each do |link|
-    puts link.content
-  end  
-
-browser.select_list(:name => 'group_3').select '8 m²'
-  doc = Nokogiri::HTML.parse(browser.html)
-  doc.xpath('//*[@id="our_price_display"]').each do |link|
-    puts link.content
-  end  
+end
